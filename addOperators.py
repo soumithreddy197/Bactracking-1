f2b82c5c7da1addffee36087b1ccfa5cd1885b1b
soumith <soumithr@buffalo.edu>
2021-02-25 18:39:28 -0500

    Finished Task

---
+++ addOperators.py
@@ -0,0 +1,30 @@
+﻿#TimeComplexity:O(N*M) NOT SURE EXACTLY
+#SpaceComplexity: O(N)
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution:
+    def backtracking(self,num,index,target,prevSum,prevAddition,exp):
+        if index>=len(num):
+            if prevSum==target:
+                self.output.append(exp)
+            return
+        
+        for i in range(index,len(num)):
+            if num[index] == '0' and i != index:
+                continue
+            currElem=int(num[index:i+1])
+            if index==0:
+                self.backtracking(num,i+1,target,prevSum+currElem,currElem,exp+str(currElem))
+            else:            
+                self.backtracking(num,i+1,target,prevSum+currElem,currElem,exp+'+'+str(currElem))
+                self.backtracking(num,i+1,target,prevSum-currElem,currElem*-1,exp+'-'+str(currElem))
+                self.backtracking(num,i+1,target,prevSum-prevAddition+prevAddition*currElem,prevAddition*currElem,exp+'*'+str(currElem))
+        
+        
+    def addOperators(self, num: str, target: int) -> List[str]:
+        
+        self.output=[]
+        self.backtracking(num,0,target,0,0,'')
+        
+        return self.output
+    
---
+++ combinationSum.py
@@ -0,0 +1,34 @@
+﻿#TimeComplexity:O(N*M) 
+#SpaceComplexity: O(N)
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution:
+         
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if candidates==None:
+            return None
+        self.output=[]
+        self.candidates=candidates
+        self.recur(target,[],0,0)
+        return self.output 
+    def recur(self,target,path,curSum,index):
+        if curSum>target or index==len(self.candidates):
+            return
+        if curSum==target:
+            self.output.append(path)
+        for i in range(index,len(self.candidates)):
+            self.recur(target,path+[self.candidates[i]],curSum+self.candidates[i],i)
+    
+'''
+    def recur(self,target,path,index,sum):
+        if sum>target or index==len(self.candidates):
+            return
+        if sum==target:
+            self.output.append(copy.deepcopy(path))
+            return
+        for i in range(index,len(self.candidates)):
+            path.append(self.candidates[i])
+            self.recur(target,path,i,sum+self.candidates[i])
+            path.pop()        
+'''
+                
